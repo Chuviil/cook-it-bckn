e15d079cc464f18a64ab7748dd0d0d85c13e13cd
Sebastian Quito <josue.quito@udla.edu.ec>
2023-11-22 20:45:27 -0500

    Moved mongo connection string to .env

--- src/database.js
+++ src/database.js
@@ -1,5 +1,6 @@
+import 'dotenv/config';
 import mongoose from "mongoose";
 
-mongoose.connect(`mongodb+srv://Chuvii:TitaniC2@learncluster.kdxcelk.mongodb.net/cookit`)
+mongoose.connect(process.env.MONGO_CONNECTION)
     .then(()=> console.log("Connected to MongoDB"))
     .catch((e) => console.log(e));
